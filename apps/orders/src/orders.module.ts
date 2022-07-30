922caf0d2e10323582099652a4a7b809053c02a2
Cormorant Programming <cormorant.programming@gmail.com>
2022-07-30 13:44:41 +0700

    order schema

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import Joi from 'joi';
+import { DatabaseModule } from 'libs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object ({
+          MONGODB_URI: Joi.string().required()
+        }),
+      envFilePath: './apps/orders/.env'
+    }),
+
+    DatabaseModule,
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
+
 export class OrdersModule {}
