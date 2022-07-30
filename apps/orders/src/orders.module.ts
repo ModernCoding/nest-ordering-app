f78275392fe562b707e2074252ac0e9c0b04e208
Cormorant Programming <cormorant.programming@gmail.com>
2022-07-30 15:16:42 +0700

    fix

--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import Joi from 'joi';
+import * as Joi from 'joi';
 import { DatabaseModule } from '@app/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
